0313fe81b179695c6f53f062cb2f8a013c89d71e
Khoa Nguyen <khoa321a@gmail.com>
2016-02-29 14:18:08 -0800

    rename

--- JsonLayout/JsonLayout/Properties/AssemblyInfo.cs
+++ JsonLayout/JsonLayout/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JsonLayout")]
+[assembly: AssemblyTitle("log4.net.Layout.JsonLayout")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JsonLayout")]
+[assembly: AssemblyProduct("log4.net.Layout.JsonLayout")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- JsonLayout/JsonLayout/log4net.Layout.JsonLayout.cs
+++ JsonLayout/JsonLayout/log4net.Layout.JsonLayout.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace log4net.Layout
+﻿namespace log4net.Layout
 {
     public class JsonLayout : log4net.Layout.LayoutSkeleton
     {
